65e47058f885ea828027186e81c1310c5bf05d26
Gustavo Martinez <115798702+GustavMartinez@users.noreply.github.com>
2024-02-04 13:33:57 -0300

    Add files via upload
    Firsts comands in Python

---
+++ Basic_concepts_1.py
@@ -0,0 +1,28 @@
+#BASIC CONCEPTS
+
+#Coments
+#Print Function
+#Input Function
+
+#This is a coment
+
+'''This 
+is also
+a coment'''
+
+#Print Function
+
+print('Show this text')
+
+print("Also, show this text")
+
+print("una linea \n otra linea")
+
+print("Concatenar " + "Un texto con " + "otro texto")
+
+
+#Input Function
+
+input('Has una pregunta: ')
+
+print("Hola! " + input("What is your name?"))
