4bf737ee5be1d54cf843b80cbb2dc7ef3e7e993d
Gustavo Martinez <115798702+GustavMartinez@users.noreply.github.com>
2024-02-05 15:03:25 -0300

    Add files via upload
    Data type explanation and examples. Type checking

---
+++ Basic_concepts_3.py
@@ -0,0 +1,27 @@
+#Data types
+
+#Strings == Texto
+print("Esto es un string")
+
+#integers == Numero enteros
+print("Esto es un int: ", 3)
+
+#float == Numeros decimales
+print("Esto es un float: ", 4.56)
+
+#Boolean == logical. True or False
+print("Esto es un bool: ", True)
+
+
+#Accesar un texto a partir de su indice
+print('hola'[3])  
+
+#Type checking:
+print(type('Holaaaa'))
+
+
+#Conversion de texto para int y verificacion:
+texto = '12345'  #variable con datos en string
+numero = int(texto)  #Uso de la funcion int() para transformar string en int
+
+print(type(numero))  #Type checking
