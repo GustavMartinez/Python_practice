2ff694faff7760eda85c3e8165b853921d59676b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-02 12:01:54 -0300

    Ejemplos de creacion de variables

---
+++ 01_basics/variables.py
@@ -0,0 +1,23 @@
+# Create a variable:
+
+x = "x is a variable"
+y = "y is another variable"
+
+print(x)
+print(y)
+
+# Many values to multiple variables:
+
+a, b, c = 'Orange', 'Banana', 'Cherry'
+
+print(a)
+print(b)
+print(c)
+
+# One value to multiple variables:
+
+m = n = o = 'My house is blue'
+
+print(m)
+print(n)
+print(o)
