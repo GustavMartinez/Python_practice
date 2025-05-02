9007abac58029cbfcdf894a97f02af04471dc593
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-02 11:48:39 -0300

    input_output functions

---
+++ 01_basics/input_output.py
@@ -0,0 +1,15 @@
+#Print Function
+
+print('Show this text')
+
+print("Also, show this text")
+
+print("una linea \n otra linea")
+
+print("Concatenar " + "Un texto con " + "otro texto")
+
+# Input Function:
+
+input('Has una pregunta: ')
+
+print("Hola! " + input("What is your name?"))
