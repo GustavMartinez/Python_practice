74a1044b06e0d2ae73c3cbe4105a3e8262cd08e7
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-03 14:53:35 -0300

    Operadores matematicos

---
+++ 01_basics/04_operadores.py
@@ -0,0 +1,36 @@
+# Operadores matemáticos
+
+
+# Operación de suma con tipos de datos 'int' y 'float'
+# Operador de suma "+"
+
+print(5+2)
+print(3.5+4.58)
+
+
+
+# Operaciones utilizando variables:
+variable_x = 54
+variable_y = 3.15487
+
+print(variable_x + variable_y)
+
+
+
+#substraction
+3-5
+
+#multiplication
+6*9
+
+#Division
+8/7
+
+#Exponentiation
+8**2
+
+#Floor division
+8//2
+
+#Module
+9%3
