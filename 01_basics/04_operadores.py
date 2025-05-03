9df5d647b1af5d3033a75111056d864e77fbf623
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-03 15:08:05 -0300

    Actualización de operadores matematicos

--- 01_basics/04_operadores.py
+++ 01_basics/04_operadores.py
@@ -4,9 +4,9 @@
 # Operación de suma con tipos de datos 'int' y 'float'
 # Operador de suma "+"
 
+print("Operación de suma: ")
 print(5+2)
 print(3.5+4.58)
-
 
 
 # Operaciones utilizando variables:
@@ -16,10 +16,10 @@ variable_y = 3.15487
 print(variable_x + variable_y)
 
 
-
 # Operación de resta con tipos de datos 'int' y 'float'
 # Operador de resta "-"
 
+print("Operación de resta: ")
 print(5-2)
 print(3.258 - 584.695)
 
@@ -31,6 +31,7 @@ print(variable_y - variable_x)
 # Operación de multiplicación con tipos de datos 'int' y 'float'
 # Operador de multiplicación "*"
 
+print("Operación de multiplicación")
 print(54*5)
 print(5.89 - 21.4)
 
@@ -42,6 +43,7 @@ print(variable_y - variable_x)
 # Operación de división con tipos de datos 'int' y 'float'
 # Operador de división "/"
 
+print("Operación de división")
 print(23/5)
 print(4.7/1.4)
 
@@ -53,15 +55,21 @@ print(variable_y - variable_x)
 # Operación de potenciación con tipos de datos 'int' y 'float'
 # Operador de potenciación "**"
 
+print("2 elevado a la 5 es:")
 dos_elevado_a_5 = 2**5
 print(dos_elevado_a_5)
 
+print("3 Elevado a la 10, es: ")
 tres_elevado_a_la_10 = 3**10
 print(tres_elevado_a_la_10)
 
 
-#Floor division
-8//2
+# Para obtener el entero de la división, se usa //. Así:
+
+print("Floor Division: ")
+print(8//2)
+print(3//2)
+
 
 #Module
 9%3
