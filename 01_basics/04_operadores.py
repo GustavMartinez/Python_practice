2eba56a067dc1a1a722bdca82d964d7481760c8c
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-03 15:02:37 -0300

    Actualizacion sobre operadores matematicos

--- 01_basics/04_operadores.py
+++ 01_basics/04_operadores.py
@@ -28,14 +28,37 @@ variable_y = 3.15487
 print(variable_y - variable_x)
 
 
-#multiplication
-6*9
+# Operación de multiplicación con tipos de datos 'int' y 'float'
+# Operador de multiplicación "*"
 
-#Division
-8/7
+print(54*5)
+print(5.89 - 21.4)
 
-#Exponentiation
-8**2
+variable_x = 32
+variable_y = 2.5487
+print(variable_y - variable_x)
+
+
+# Operación de división con tipos de datos 'int' y 'float'
+# Operador de división "/"
+
+print(23/5)
+print(4.7/1.4)
+
+variable_x = 20
+variable_y = 3.17
+print(variable_y - variable_x)
+
+
+# Operación de potenciación con tipos de datos 'int' y 'float'
+# Operador de potenciación "**"
+
+dos_elevado_a_5 = 2**5
+print(dos_elevado_a_5)
+
+tres_elevado_a_la_10 = 3**10
+print(tres_elevado_a_la_10)
+
 
 #Floor division
 8//2
