adba2217d347ad47e21f82ed83853709d34de4f4
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-04 16:09:13 -0300

    alteracion en funciones en general y funciones matematicas

--- 01_basics/06_some_maths.py
+++ 01_basics/06_some_maths.py
@@ -1,4 +1,5 @@
 
+# Build-in functions for math operations:
 
 # round()
 # abs()
@@ -6,5 +7,14 @@
 # max()
 # min()
 
-# import math
+
+
+# Python has a math module that provides most of the familiar mathematical functions. 
+# A module is a file that contains a collection of related functions.
+
+
+#Before we can use the functions in a module, we have to import it with an import statement:
+import math
+
+# Para usar las funciones del modulo, hay que especificar el modulo y el nombre de la función, serparados por un punto. A esto se le llama (dot notation)
 
--- 03_functions/01_intro_to_functions.py
+++ 03_functions/01_intro_to_functions.py
@@ -1,2 +1,21 @@
 # a function is a named sequence of statements that performs a computation.
 
+# Example of a function call:
+
+print(42)
+
+# Example of two function calls:
+
+print(type(42))
+
+# Example of other functions:
+
+"""
+type()
+int()
+float()
+str()
+
+"""
+
+# This functions take an argument and returns a result.
