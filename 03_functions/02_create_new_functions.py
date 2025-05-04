97f3e049d23921df753a96970a376037598ba168
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-04 16:27:48 -0300

    creando nuevas funciones

---
+++ 03_functions/02_create_new_functions.py
@@ -0,0 +1,22 @@
+# A function definition specifies the name of a new function and the sequence of statements that run when the function is called.
+
+# Example of a new function:
+
+def print_lyrics():
+    print("So close, no matter how far")
+    print("Couldn't be much more from the heart")
+    print("Forever trusting who we are")
+    print("And nothing else matters")
+
+print_lyrics()
+
+
+# Once you have defined a function, you can use it inside another function.
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    print_lyrics()
+    
+
+repeat_lyrics()
