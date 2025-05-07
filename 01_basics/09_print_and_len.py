5aa058dd772e5d7e213c141517b073fa991887a1
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-06 22:06:11 -0300

    Actualziacion print y len

---
+++ 01_basics/09_print_and_len.py
@@ -0,0 +1,18 @@
+print("Today is Monday")
+print("I like string beans")
+
+print("Today", "is", "Monday")
+print("Today", "is", "Monday", sep="...")
+
+print("Today is Monday, ", end="")
+print("i like string beans.")
+
+print("Today", "is", "Monday", sep="?", end="!!")
+print("I like string beans.")
+
+
+# Get the length of a string:
+
+name = "Gustavo"
+lenght = len(name)
+print(f"The lenght of the string '{name}' is: {lenght}")
