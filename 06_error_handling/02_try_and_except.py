abe45969b45076e5a302fc01d2dbaaa829a46249
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-06 22:21:40 -0300

    actualizacion try and except

---
+++ 06_error_handling/02_try_and_except.py
@@ -0,0 +1,11 @@
+# Division by zero
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("Error: Division by zero!")
+
+
+# try - except - else - finaly
+
+# finaly will execute no matter what
