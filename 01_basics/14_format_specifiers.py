a488283cecb102042d227ae434466feb9fb4fee6
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-12 23:53:46 -0300

    Actualizacion de format specifiers

--- 01_basics/14_format_specifiers.py
+++ 01_basics/14_format_specifiers.py
@@ -2,8 +2,8 @@
 # structure: {value:flags}
 
 # EXAMPLE 1
-value_1 = 3.14159
-value_2 = -985.98765
+value_1 = 3000.14159
+value_2 = -9850.98765
 value_3 = 12.35
 
 print(f"Value 1 is {value_1:.2f}") # Flag indicando 2 decimales
@@ -39,3 +39,9 @@ print(f"Value 3 is {value_3:>10}") # Flag indicando 10 espacios para el output y
 print(f"Value 1 is {value_1:^20}") # Flag indicando 20 espacios para el output y alineado al centro
 print(f"Value 2 is {value_2:^20}") # flag indicando 20 espacios para el output y alineado al centro
 print(f"Value 3 is {value_3:^20}") # Flag indicando 20 espacios para el output y alineado al centro
+
+# EXAMPLE 7
+
+print(f"Value 1 is {value_1:,}") # Flag indicando separador de miles
+print(f"Value 2 is {value_2:,}") # flag indicando separador de miles
+print(f"Value 3 is {value_3:,}") # Flag indicando separador de miles
