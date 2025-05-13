000f10737ea543eee3732497c55b94c18091c3b7
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-12 23:58:14 -0300

    Actualizacion format specifiers

--- 01_basics/14_format_specifiers.py
+++ 01_basics/14_format_specifiers.py
@@ -45,3 +45,9 @@ print(f"Value 3 is {value_3:^20}") # Flag indicando 20 espacios para el output y
 print(f"Value 1 is {value_1:,}") # Flag indicando separador de miles
 print(f"Value 2 is {value_2:,}") # flag indicando separador de miles
 print(f"Value 3 is {value_3:,}") # Flag indicando separador de miles
+
+# EXAMPLE 8 - MIXING
+
+print(f"Value 1 is {value_1:>20,.2f}") # Flag indicando separador de miles
+print(f"Value 2 is {value_2:>20,.2f}") # flag indicando separador de miles
+print(f"Value 3 is {value_3:>20,.2f}") # Flag indicando separador de miles
