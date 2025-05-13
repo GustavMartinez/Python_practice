3906f014b47996b4369583e84282a31c3b9d07f8
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-12 23:25:05 -0300

    format specifiers

---
+++ 01_basics/14_format_specifiers.py
@@ -0,0 +1,11 @@
+# Formar a value based on what flags are inserted
+# structure: {value:flags}
+
+# EXAMPLE 1
+value_1 = 3.14159
+value_2 = -985.98765416
+value_3 = 12.354321
+
+print(f"Value 1 is {value_1:.2f}") # Flag indicando 2 decimales
+print(f"Value 2 is {value_2:.3f}") # flag indicando 3 decimales
+print(f"Value 3 is {value_3:.4f}") # Flag indicando 4 decimales
