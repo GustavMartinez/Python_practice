879496fa6b677ca4c9dca84f5b0ba11995564181
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-18 17:17:12 -0300

    for loops 1

---
+++ 02_control_flow/12_for_loops_01.py
@@ -0,0 +1,17 @@
+# for loops = execute a block of code a fixed number of times.
+# you can iterate over a range, string, sequence, etc.
+
+for counter in range(1, 11):
+    print(counter)
+
+print("Feliz año navidad!")
+
+
+for counter in reversed(range(1, 11)):
+    print(counter)
+
+print("Feliz año nuevo!")
+
+
+for counter in range(1, 11, 2):
+    print(counter)
