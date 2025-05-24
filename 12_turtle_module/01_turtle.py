1cf553040b7492d78ec7ded708a8880e93db34dd
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-24 16:06:45 -0300

    creando a bob

--- 12_turtle_module/01_turtle.py
+++ 12_turtle_module/01_turtle.py
@@ -1,10 +1,21 @@
 import turtle
 from turtle import Screen
 
+
+# Creando a bob
 bob = turtle.Turtle()
 
+
+# Haciendo que bob haga un cuadrado
 print(bob)
 
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
 
 
 my_screen = Screen()
