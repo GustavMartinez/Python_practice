f08f9ee93398d85ddd0945fef6e330f51ea1be54
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-25 17:28:00 -0300

    Actualizacion de funciones con muchos argumentos

--- 03_functions/06_fuctions_args.py
+++ 03_functions/06_fuctions_args.py
@@ -5,11 +5,22 @@ def list_of_numbers(*numbers):
     for number in numbers:
         print(number)
 
+def multiply(*numbers):
+    """
+    Esta funcion multiplica todos los números que se le pasen
+    """
+    total = 1
+    for number in numbers:
+        total*= number
+    return total
 
 
 
 
-
-
+# Primera funcion
 
 list_of_numbers(2,5,3,6,8)
+
+# Segunda funcion:
+print()
+print(multiply(5,8,4,2))
