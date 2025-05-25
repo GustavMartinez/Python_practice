1cb06c5638edff5baaeab54fd0a7ba4b6d3d1a72
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-05-25 16:49:17 -0300

    Practica de funciones

---
+++ 03_functions/04_functions_basics_2.py
@@ -0,0 +1,24 @@
+# retunr = statement used to end a function and send a result back to the caller
+
+# simple calculator
+
+def addition(num_1, num_2):
+    return num_1 + num_2
+
+
+def subtraction(num_1, num_2):
+    return num_1 - num_2
+
+
+def multiplication(num_1, num_2):
+    return num_1 * num_2
+
+
+def division(num_1, num_2):
+    return num_1 / num_2
+
+
+print(addition(2,5))
+print(subtraction(50,80))
+print(multiplication(20,8))
+print(division(5,8))
