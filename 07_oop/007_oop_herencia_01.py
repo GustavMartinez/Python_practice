debe162c087021cdee4b0fc3c31bc0102d01872f
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-03 07:48:40 -0300

    herencia multiple

--- 07_oop/007_oop_herencia_01.py
+++ 07_oop/007_oop_herencia_01.py
@@ -16,10 +16,43 @@ class Empleado(Persona):
         self.salario = salario
 
 
-#roberto = Empleado("Roberto", 43, "argentino")
-pablo = Persona("Pablo", 52, "chileno")
-ricardo = Empleado("ricardo", 43, "espanol", 'programador', 15000)
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, nacionalidad, notas, universidad):
+        super().__init__(nombre, edad, nacionalidad)
+        self.notas = notas
+        self.universidad = universidad
 
-print(ricardo.edad)
-print(ricardo.nacionalidad)
-ricardo.hablar()
+
+class Artista:
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+
+    def mostrar_habilidad(self):
+        return f"Mi habilidad es: {self.habilidad}"
+
+
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad, nacionalidad, habilidad, empresa, salario):
+        Persona.__init__(self, nombre, edad, nacionalidad)
+        Artista.__init__(self, habilidad)
+        self.salario = salario
+        self.empresa = empresa
+
+    def presentarse(self):
+        print( f'Hola, soy: {self.nombre}, {self.mostrar_habilidad()} y trabajo en {self.empresa}')
+
+
+#roberto = Empleado("Roberto", 43, "argentino")
+#pablo = Persona("Pablo", 52, "chileno")
+#ricardo = Empleado("ricardo", 43, "espanol", 'programador', 15000)
+
+#print(ricardo.edad)
+#print(ricardo.nacionalidad)
+#ricardo.hablar()
+
+
+# con las nuevas clases:
+
+roberto = EmpleadoArtista("Roberto", 43, "argentino", "cantar", "Google", 15000)
+
+roberto.presentarse()
