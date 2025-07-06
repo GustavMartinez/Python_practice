f5ecfd655242e3aeb187a68d430b197b2f438d13
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-06 06:03:42 -0300

    Actualizacion de string methods

--- 01_basics/12_string_methods.py
+++ 01_basics/12_string_methods.py
@@ -3,32 +3,32 @@ name = input("Enter your full name: ")
 
 # Function to count the number of characters, including spaces
 len_name = len(name) # Len with spaces
-print(len_name)
+print(f"El número de caracteres es: {len_name}")
 
 # Look for the first apparence of a character:
 character = name.find("t") # Esto busca por la letra o en el nombre, y devuelve el index.
-print(character)
+print(f"La primera 't', aparece en el indice: {character}")
 
 
 # Look for the last apparence of a character:
 character = name.rfind("t") # Esto busca por la última posición de la letra t y devuelve el index
-print(character)
+print(f"La última 't' que aparece en el texto esta en el indice: {character}")
 
 # If there is no character in the string, the result is -1
 
 # Capitalize the first letter of the string
 capital = name.capitalize()
-print(capital)
+print(f"Primera letra capitalizada: {capital}")
 
 
 # Make all uppercase
 all_upper = name.upper()
-print(all_upper)
+print(f"Convertir todo el texto a mayuscula: {all_upper}")
 
 
 #Make all lowercase
 all_lower = name.lower()
-print(all_lower)
+print(f"Convertir todo el texto a minuscula: {all_lower}")
 
 # Replace method:
 replace_name = name.replace("m", 'n')
