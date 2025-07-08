c63841f3b2f68ce8f01470d423f57e8d7d65b1a2
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-08 07:48:17 -0300

    operadores y type conversion

---
+++ 13_100_days_of_code/Day_02/lesson_02.py
@@ -0,0 +1,51 @@
+# Type check
+
+print(type("Hola"))
+print(type(123554))
+print(type(12.584))
+print(type(True))
+
+print()
+# Type conversion or type casting
+
+print(int("123456"))
+print(str(123456))
+print(bool(123))
+
+print()
+#Mathematical operations
+
+# suma
+
+print("Suma")
+print(1 + 2)
+
+
+# resta
+
+print("Resta")
+print(5-2)
+
+
+# Multiplicación
+
+print("multiplicación")
+print(5*9)
+
+
+# Division
+
+print("División")
+print(5/6)
+
+
+# Floor division
+
+print("Divison entera")
+print(5//2)
+
+
+# Exponents
+
+print("Exponencial")
+print(2**3)
