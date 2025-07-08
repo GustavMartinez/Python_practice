29ef6e70fa9d771b653863dc06e86f712cf813bb
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-08 07:15:10 -0300

    day1 of 100 days of python

---
+++ 13_100_days_of_code/Day_01/day_project.py
@@ -0,0 +1,12 @@
+# Band name generator
+
+print("-- Welcome to the Band Name Generator! --")
+print("-----------------------------------------")
+
+city = input("What's the name of the city you grew up in? \n")
+pet = input("what's your pet's name?\n")
+
+print(f"Your band name could be: {city} {pet}")
+
+print("----------------")
+print("program finished")
---
+++ 13_100_days_of_code/Day_01/leason_02.py
@@ -0,0 +1,10 @@
+nombre_usuario = input("Ingresa tu nombre: ")
+
+print(f"Tu nombre es: {nombre_usuario}!")
+
+
+#Ejercicio
+
+username = input("What is your name: ")
+length = len(username)
+print(length)
