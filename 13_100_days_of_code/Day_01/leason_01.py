65725f6274a233111cdacca36868c5cd349d7289
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-07 21:58:54 -0300

    revision del curso de python

---
+++ 13_100_days_of_code/Day_01/leason_01.py
@@ -0,0 +1,18 @@
+# Hacer un print :)
+# Ver errores
+# ver cuando aparecen sublineas, indican que hay algun error
+
+
+print("Hola mundo")
+print("----------")
+print("De nuevo Gustavo por aqui")
+print("----------")
+
+print("Hola", "mundo", sep="\n")
+print("hola", "mundo", "nuevo", end="\t")
+
+print("Gustavo\nAdolfo")
+
+
+# String concatenation
+print("Hola" + " " +"Adolfo")
