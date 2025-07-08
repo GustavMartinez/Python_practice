d465ad6cb1c9f027b0594dca32fe8c706acb5643
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-08 07:29:44 -0300

    Actualizacion de nombres de archivos y nueva clase

---
+++ 13_100_days_of_code/Day_02/lesson_01.py
@@ -0,0 +1,27 @@
+# strings
+# Subscripting
+
+print("Hola"[0])
+print("Hola"[1])
+print("Hola"[2])
+print("Hola"[-1])
+
+print()
+
+# Integers
+
+print(123+321)
+
+print()
+
+# Floats
+
+print(1.5 + 3.2)
+
+print()
+# Boolean
+
+print(True)
+print(False)
+
+print(type(734_456.584))
