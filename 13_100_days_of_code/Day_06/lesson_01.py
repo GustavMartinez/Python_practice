9c8b09ce67a75067b99cfc1176f75184b2d8883e
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-12 04:33:22 -0300

    mas sobre funciones

--- 13_100_days_of_code/Day_06/lesson_01.py
+++ 13_100_days_of_code/Day_06/lesson_01.py
@@ -4,3 +4,16 @@ def my_function():
 
 
 my_function()
+
+
+def funcion_sumar(a,b):
+    return a + b
+
+
+num_1 = int(input("Ingresa el primer valor para sumar: "))
+num_2 = int(input("Ingresa el segundo valor para sumar: "))
+
+
+suma = funcion_sumar(num_1, num_2)
+
+print(f"El resultado de la suma es: {suma}")
