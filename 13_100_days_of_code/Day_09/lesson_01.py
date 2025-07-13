382b52b7a10846c5f16749106ce2f35236d8f605
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-13 17:35:21 -0300

    Dictionaryes lesson 1 and 2

---
+++ 13_100_days_of_code/Day_09/lesson_01.py
@@ -0,0 +1,40 @@
+# Dictionaries
+
+my_dictionary = {
+    "element_1":"Value x",
+    "element_2":"value y",
+    "element_3":"value z"
+}
+
+for i in my_dictionary.values():
+    print(i)
+
+for j in my_dictionary.keys():
+    print(j)
+
+for k, v in my_dictionary.items():
+    print(k, v)
+
+print(my_dictionary["element_1"])
+
+
+# Add new entry:
+
+my_dictionary["element_4"] = "value_a"
+
+
+for k, v in my_dictionary.items():
+    print(k,v)
+
+
+# Creating and empty dictionary:
+
+my = {}
+
+
+# Edit an item in a dictionary:
+
+my_dictionary["element_1"] = "value b"
+
+
+print(my_dictionary["element_1"])
---
+++ 13_100_days_of_code/Day_09/lesson_02.py
@@ -0,0 +1,41 @@
+
+"""
+
+Write a program that converts their scores to grades.
+
+This is the scoring criteria: 
+
+- Scores 91 - 100: Grade = "Outstanding" 
+
+- Scores 81 - 90: Grade = "Exceeds Expectations" 
+
+- Scores 71 - 80: Grade = "Acceptable" 
+
+- Scores 70 or lower: Grade = "Fail" 
+
+"""
+
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+
+for k,v in student_scores.items():
+    if v <= 70:
+        student_grades[k] = "Fail"
+    elif v > 70 and v < 80:
+        student_grades[k] = "Acceptable"
+    elif v > 80 and v < 90:
+        student_grades[k] = "Exceeds Expectations"
+    elif v > 90 and v < 100:
+        student_grades[k] = "Outstanding"
+
+
+print(student_grades)
