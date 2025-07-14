7530db235d6224f8ec9b46f3e15e556d6ca2fc24
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-14 19:46:34 -0300

    day project

--- 13_100_days_of_code/Day_09/day_project.py
+++ 13_100_days_of_code/Day_09/day_project.py
@@ -1,6 +1,30 @@
 print("Welcome to the secret auction program.")
 
-name = input("What is your name?:\n")
-bid = input("What's your bid?:\n$")
+bids = {}
 
-more_bidders = input("Are there any other bidders? Type 'yes' or 'no'")
+more_bidders = True
+
+while more_bidders == True:
+
+    name = input("What is your name?:\n")
+    bid = int(input("What's your bid?:\n$"))
+
+    bids[name] = bid
+
+    
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'").lower()
+    if other_bidders == "no":
+        more_bidders = False
+    elif other_bidders == "yes":
+        print("\n"*100)
+
+highest = 0
+winner = ''
+
+for k,v in bids.items():
+    if v > highest:
+        highest = v
+        winner = k
+
+print(bids)
+print(f"The winner is: {winner}, the bid was: {highest}")
