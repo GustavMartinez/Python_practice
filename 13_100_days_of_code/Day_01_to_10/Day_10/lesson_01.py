5df38ca0ff1443a30a5f8a714786afd8287373a5
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-15 19:58:30 -0300

    actualizacion

--- 13_100_days_of_code/Day_01_to_10/Day_10/lesson_01.py
+++ 13_100_days_of_code/Day_01_to_10/Day_10/lesson_01.py
@@ -4,3 +4,29 @@
 def format_name(f_name, l_name):
     f_name = f_name.title()
     l_name = l_name.title()
+
+
+def format(f_name, l_name):
+    return f"El nombre es: {f_name}, y el sobre nombre es: {l_name}"
+
+
+nombre = format("Gustavo", "martinez")
+
+print(nombre)
+
+
+
+# New version:
+
+def format_name_2(f_name, l_name):
+
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
+
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+
+print(format_name_2(input("What is your first name?"), input("What is your last name?")))
