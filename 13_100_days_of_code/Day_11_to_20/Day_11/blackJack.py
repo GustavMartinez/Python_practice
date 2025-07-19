995bfdad130d84f910ea2875eace54b208d1ec9c
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-19 07:22:29 -0300

    actualizacion y comentarios del codigo hasta el momento. hay un nuevo desafio que necesito resolver

--- 13_100_days_of_code/Day_11_to_20/Day_11/blackJack.py
+++ 13_100_days_of_code/Day_11_to_20/Day_11/blackJack.py
@@ -1,36 +1,38 @@
+# Importar la biblioteca
 import random
 
+# Lista de las cartas en el set de cartas
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 print()
 #initialize = input("Do you want to play a game of Blackjack? Type 'y' or 'n': \t")
 
-
+# Creacion de la lista de las cartas del computador y el usuario. listas vacias
 player_hand = []
-computer_hand = [5,11,7]
+computer_hand = []
 
+
+# seleccion aleatoria de las cartas iniciales del pc y del jugador
 for i in range(2):
     player_hand.append(random.choice(cards))
-    # computer_hand.append(random.choice(cards))
-
-
-
-while sum(computer_hand) < 17:
-    if 11 in computer_hand and sum(computer_hand) > 21:
-        computer_hand.remove(11)
-        computer_hand.append(1)
     computer_hand.append(random.choice(cards))
 
 
+# condicion para que el computador elija una carta mas, si las actuales cartas suman menos que 17
+while sum(computer_hand) < 17:
+    computer_hand.append(random.choice(cards))
+
+# Mostrar las cartas del pc y la sumatoria de las mismas
 print(f"{computer_hand}, computer score: {sum(computer_hand)}")
 
 
 
 more_cards = True
 
+# Condicion para que el usuario elija o no más cartas e indique si se paso de 21
 while more_cards:
 
     print(f"Your cards: {player_hand}, current score: {sum(player_hand)}")
-    print(f"computer's firts card: {computer_hand[0]}")
+    print(f"computer's firts card: {computer_hand[0]}") # mostrar la primera carta del pc
     
     add = input("Try 'y' to get another card, type 'n' to pass: \t")
 
@@ -47,8 +49,6 @@ while more_cards:
     if sum(player_hand) > 21:
        more_cards = False
        print(f"Your cards: {player_hand}, You lose. Your final score is {sum(player_hand)}")
-    
-    
-    
 
-    
+
+# problema: convertir el 11 en 1
