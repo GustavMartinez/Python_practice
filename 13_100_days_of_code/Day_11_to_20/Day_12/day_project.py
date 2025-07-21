31f8865b15e67185b8ac6b5da6b1a6d419a13c85
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-21 07:36:09 -0300

    nuevas funciones para la logica

--- 13_100_days_of_code/Day_11_to_20/Day_12/day_project.py
+++ 13_100_days_of_code/Day_11_to_20/Day_12/day_project.py
@@ -1,9 +1,15 @@
+import random
+
 print("------------------------------------")
 print("Welcome to the Number Guessing Game!")
 print("------------------------------------")
 
 
 # Functions
+
+def number_to_guess():
+    return random.randint(1,100)
+
 def difficulty():
 
     level = input("Choose a difficulty. Type 'easy' or 'hard':\n").lower()
@@ -16,10 +22,21 @@ def difficulty():
         print('not a valid input. Try again')
         difficulty()
     
+def attemps(user_difficulty, number):
+    
+    if user_difficulty == 'easy':
+        print("You have 5 attempts remaining to guess the number")
+        print(f"The number is {number}")
+    else:
+        print("you have 10 attempts remaining to guess the number")
+        print(f"The number is {number}")
+
 
 
 # not functions
 print("I'm thinking of a number between 1 and 100.")
 
 
-difficulty()
+user_difficulty = difficulty()
+number = number_to_guess()
+attemps(user_difficulty, number)
