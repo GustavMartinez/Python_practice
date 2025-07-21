45bbda48ac4a765e19b4569696522410597b0076
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-07-21 07:07:14 -0300

    logica inicial: recibir los parametros correctos"

--- 13_100_days_of_code/Day_11_to_20/Day_12/day_project.py
+++ 13_100_days_of_code/Day_11_to_20/Day_12/day_project.py
@@ -3,4 +3,23 @@ print("Welcome to the Number Guessing Game!")
 print("------------------------------------")
 
 
+# Functions
+def difficulty():
 
+    level = input("Choose a difficulty. Type 'easy' or 'hard':\n").lower()
+
+    if level == 'easy':
+        return 'easy'
+    elif level == 'hard':
+        return 'hard'
+    else:
+        print('not a valid input. Try again')
+        difficulty()
+    
+
+
+# not functions
+print("I'm thinking of a number between 1 and 100.")
+
+
+difficulty()
