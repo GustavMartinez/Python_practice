905fd5b7d02659e3abf5c2b9161c434d980e0295
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-09 14:45:52 -0300

    dashed line and square

--- 13_100_days_of_code/Day_11_to_20/Day_18/turtle_dashed_line.py
+++ 13_100_days_of_code/Day_11_to_20/Day_18/turtle_dashed_line.py
@@ -1,10 +1,23 @@
+
+# Dashed line and square
+
 from turtle import Turtle, Screen
 
 tommy = Turtle()
+tommy.shape("turtle")
+tommy.color("green")
 
 
+def linea():
+    for _ in range(10):
+        tommy.fd(10)
+        tommy.penup()
+        tommy.fd(10)
+        tommy.pendown()
 
-
+for _ in range(4):
+    linea()
+    tommy.right(90)
 
 
 mi_ventana = Screen()
