a4ffb125685d461397e05f809e2f2c8c874eea0d
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-10 17:38:51 -0300

    Hirst painting second option

--- 13_100_days_of_code/Day_11_to_20/Day_18/07_day_project_option_2.py
+++ 13_100_days_of_code/Day_11_to_20/Day_18/07_day_project_option_2.py
@@ -11,21 +11,27 @@ t.colormode(255)
 
 color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51), (6, 68, 42), (176, 176, 233), (239, 168, 161), (249, 8, 48), (5, 246, 222), (15, 76, 110), (243, 15, 14), (38, 43, 221)]
 
-punto = t.Turtle()
-punto.penup()
-initial_x = -300
-initial_y = -150
+tim = t.Turtle()
+tim.speed('fastest')
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.fd(250)
+tim.setheading(0)
 
-for i in range(10):
-    punto.setpos(initial_x, initial_y)
+number_of_dots = 101
 
-    for _ in range(10):
-        col = r.choice(color_list)
-        punto.showturtle()
-        punto.dot(20, col)
-        punto.hideturtle()
-        punto.fd(50)
-    initial_y += 50
 
-ventana = t.Screen()
-ventana.exitonclick()
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, r.choice(color_list))
+    tim.fd(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = t.Screen()
+screen.exitonclick()
