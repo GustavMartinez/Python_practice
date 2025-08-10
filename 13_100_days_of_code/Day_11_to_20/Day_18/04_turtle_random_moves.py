dafe5df230bf3c0fb334313fae851b2747609289
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-10 07:43:46 -0300

    random walk

--- 13_100_days_of_code/Day_11_to_20/Day_18/04_turtle_random_moves.py
+++ 13_100_days_of_code/Day_11_to_20/Day_18/04_turtle_random_moves.py
@@ -4,14 +4,22 @@ import random
 colors = ["dark violet", "maroon", "aquamarine", "green", 'red', "goldenrod"]
 
 
+
 timmy = Turtle()
 timmy.pensize(width=10)
 
 
-for _ in range(5):
+for _ in range(100):
     timmy.color(random.choice(colors))
-    timmy.fd(50)
-    timmy.right(90)
+    timmy.fd(25)
+    direction = random.randint(0,3)
+    if direction == 0:
+        timmy.rt(90)
+    elif direction == 1:
+        timmy.lt(90)
+    elif direction == 2:
+        timmy.back(25)
+
 
 
 
