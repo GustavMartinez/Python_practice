154964c4bdc414d07113d815e0bdeccccee644a4
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-11 20:40:23 -0300

    snake game part 2

--- 13_100_days_of_code/Day_11_to_20/Day_20/snake_game_part_1.py
+++ 13_100_days_of_code/Day_11_to_20/Day_20/snake_game_part_1.py
@@ -5,8 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My snake game')
 
+starting_positions = [(0,0), (-20,0), (-40,0)]
 
 
+for position in starting_positions:
+    segment = Turtle('square')
+    segment.color('white')
+    segment.goto(position)
 
 
 
