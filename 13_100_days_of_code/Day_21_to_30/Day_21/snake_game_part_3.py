9ff53954e89b01daf37c507fe0708cc5518b1550
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-11 22:34:59 -0300

    day 21

---
+++ 13_100_days_of_code/Day_21_to_30/Day_21/snake_game_part_3.py
@@ -0,0 +1,39 @@
+from turtle import Screen
+from snake_game_part_3_snake import Snake
+from snake_game_part_3_food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My snake game')
+screen.tracer(0) # turn of tracer
+
+
+snake = Snake()
+food = Food()
+
+
+# part 3
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
+
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update() # Update screen
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+screen.exitonclick()
