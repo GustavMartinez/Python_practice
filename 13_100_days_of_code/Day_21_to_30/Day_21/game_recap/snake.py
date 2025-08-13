796a1ecc992d80e5af7bec4f46efba17639ed8a3
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-12 21:49:07 -0300

    snake recap 2

--- 13_100_days_of_code/Day_21_to_30/Day_21/game_recap/snake.py
+++ 13_100_days_of_code/Day_21_to_30/Day_21/game_recap/snake.py
@@ -1,14 +1,17 @@
 from turtle import Turtle
 
 STARTING_POSITION = [(0,0), (-20, 0), (-40,0)]
-
+MOVE_DISTANCE = 20
 
 
 class Snake:
     
     def __init__(self):
         self.segments = []
+        self.create_snake()
 
+
+    def create_snake(self):
         for position in STARTING_POSITION:
             segment = Turtle('square')
             segment.color('white')
@@ -21,4 +24,20 @@ class Snake:
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+
+    def right(self):
+        self.segments[0].setheading(0)
+
+
+    def left(self):
+        self.segments[0].setheading(180)
