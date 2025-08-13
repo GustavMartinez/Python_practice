ce3c0ead8c13cfb6b4141aedc2aad8932019db6f
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-13 18:58:59 -0300

    snake game recap 2 part 3

--- 13_100_days_of_code/Day_21_to_30/Day_21/snake_game_recap_2/snake.py
+++ 13_100_days_of_code/Day_21_to_30/Day_21/snake_game_recap_2/snake.py
@@ -2,6 +2,12 @@ from turtle import Turtle
 
 
 STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
 
 class Snake:
     def __init__(self):
@@ -23,4 +29,21 @@ class Snake:
             new_x = self.segments[seg_num-1].xcor()
             new_y = self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
+    def up(self):
+         if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+    
+    def down(self):
+         if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+    
+    def left(self):
+         if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(180)
+    
+    def right(self):
+         if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(0)
