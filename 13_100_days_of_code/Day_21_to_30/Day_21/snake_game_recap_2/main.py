a33341f8582341be6dc4ef5635d63a78fe7fe278
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-13 18:39:04 -0300

    snake game recap 2 part 2

--- 13_100_days_of_code/Day_21_to_30/Day_21/snake_game_recap_2/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_21/snake_game_recap_2/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,15 +9,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 
-starting_position = [(0,0), (-20,0), (-40,0)]
-segments = []
-
-for position in starting_position:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(position)
-    segments.append(segment)
+snake = Snake()
 
 
 game_is_on = True
@@ -25,12 +18,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.5)
 
-    for seg_num in range(len(segments)-1,0,-1): # range(start, stop, step)
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].right(90)
+    snake.move()
 
 
 screen.exitonclick()
