8e3ee6eb8e99f922fab6bd1078590b837a24bad7
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-16 06:52:35 -0300

    updates

---
+++ 13_100_days_of_code/Day_21_to_30/Day_24/lesson_1/main.py
@@ -0,0 +1,11 @@
+path = '/home/gustavo/Documents/Study/005_Python_General/github/python_practice/13_100_days_of_code/Day_21_to_30/Day_24/lesson_1/my_file.txt'
+
+
+#with open(path) as file:
+#    contents = file.read()
+#    print(contents)
+
+
+with open ('new_file.txt', mode='a') as file:
+    file.write("\nNew text")
+
