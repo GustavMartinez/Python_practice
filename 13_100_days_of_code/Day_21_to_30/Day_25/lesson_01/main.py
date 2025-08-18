beb819a0dd6f1d0524063fa4d0b8eb93c7447b05
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-08-18 10:21:41 -0300

    day 25 reorganization

--- 13_100_days_of_code/Day_21_to_30/Day_25/lesson_01/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_25/lesson_01/main.py
@@ -1,23 +1,11 @@
 
-PATH_FILE = '/home/gustavo/Documents/Study/005_Python_General/github/python_practice/13_100_days_of_code/Day_21_to_30/Day_25/lesson_01/weather_data.csv'
+PATH_FILE = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/weather_data.csv'
 
 
-#w_data = []
+w_data = []
 
-#with open (PATH_FILE) as file:
-    #data = file.readlines()
-#print(data)
+with open (PATH_FILE) as file:
+    data = file.readlines()
+print(data)
 
 
-
-import csv
-
-with open(PATH_FILE) as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    t = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-
-    print(temperatures)
---
+++ 13_100_days_of_code/Day_21_to_30/Day_25/lesson_02/main.py
@@ -0,0 +1,14 @@
+import csv
+
+PATH_FILE = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/weather_data.csv'
+
+
+# Extract data from column 'temp'
+with open(PATH_FILE) as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
