392c599bdd67caf860618b35802cddacc68e9f3d
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-08-25 19:51:41 -0300

    day project 25 recap, logic

--- 13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/main.py
@@ -1,7 +1,11 @@
 import turtle
+import pandas as pd
 
 # path to image
 PATH_IMG = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/blank_states_img.gif'
+
+# path to csv file
+PATH_CSV = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/50_states.csv'
 
 
 # Creating the image background
@@ -12,7 +16,25 @@ screen.addshape(image)
 turtle.shape(image)
 
 
+# Get csv file in pandas
+data = pd.read_csv(PATH_CSV)
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+
+    # get answer from user:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="what's another state's name?").title()
+    print(answer_state)
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
 
 
 
