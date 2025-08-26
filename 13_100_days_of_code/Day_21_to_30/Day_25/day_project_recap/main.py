d3ba9a4227213ef31eac8fcae0550adfe04239da
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-08-26 06:02:24 -0300

    day project 25 ending

--- 13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/main.py
@@ -7,6 +7,9 @@ PATH_IMG = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30
 # path to csv file
 PATH_CSV = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/50_states.csv'
 
+# path to save file with missing guesses:
+PATH_TO_SAVE = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_25/day_project_recap/'
+
 
 # Creating the image background
 screen = turtle.Screen()
@@ -27,6 +30,16 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="what's another state's name?").title()
     print(answer_state)
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv(f"{PATH_TO_SAVE}states_to_learn.csv")
+        break
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -37,5 +50,3 @@ while len(guessed_states) < 50:
         t.write(answer_state)
 
 
-
-screen.exitonclick()
