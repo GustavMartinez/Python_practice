e1e844a94dd5d58c4566da779bbd73745eef2187
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-08-30 08:19:08 -0300

    day 26 lesson 1 and 2

--- 13_100_days_of_code/Day_21_to_30/Day_26/lesson_02/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_26/lesson_02/main.py
@@ -27,3 +27,27 @@ IMPORTANT:  The output should be a list of integers and not strings!
 
 Try to use List Comprehension instead of a Loop. 
 """
+
+PATH_FILE_1 = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_26/lesson_02/file1.txt'
+PATH_FILE_2 = '/home/gustavo/repos/Python_practice/13_100_days_of_code/Day_21_to_30/Day_26/lesson_02/file2.txt'
+
+file_1 = []
+file_2 = []
+
+with open(PATH_FILE_1) as file:
+    numbers = file.readlines()
+    for n in numbers:
+        n = int(n.rstrip())
+        file_1.append(n)
+
+
+with open(PATH_FILE_2) as file2:
+    other_numbers = file2.readlines()
+    for i in other_numbers:
+        i = int(i.rstrip())
+        file_2.append(i)
+
+# result = [new for item in list if condition]
+
+result = [i for i in file_1 if i in file_2 ]
+print(result)
