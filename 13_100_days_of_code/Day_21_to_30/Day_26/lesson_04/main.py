2e4ada3331101de721211931debe63642579ab1b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-30 14:41:07 -0300

    day 26 lessons and exercises

---
+++ 13_100_days_of_code/Day_21_to_30/Day_26/lesson_04/main.py
@@ -0,0 +1,17 @@
+# Loop through rows of a data frame
+
+import pandas as pd
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56,76,98]
+}
+
+
+student_data_frame = pd.DataFrame(student_dict)
+
+print(student_data_frame)
+
+# loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(row.score)
