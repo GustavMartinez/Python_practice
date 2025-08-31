b2e0c5734ab53dd092c370803d2f364e811a5bc9
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-08-31 06:36:11 -0300

    dictionary from dataframe

--- 13_100_days_of_code/Day_21_to_30/Day_26/day_project/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_26/day_project/main.py
@@ -1,26 +1,21 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
+"""
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# Todo 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# Todo 2. Create a list of the phonetic code words from a word that the user inputs.
+
+"""
+
+import pandas as pd
+
+PATH_FILE = '/home/gustavo/Documents/Study/005_Python_General/github/python_practice/13_100_days_of_code/Day_21_to_30/Day_26/day_project/nato_phonetic_alphabet.csv'
+
+file = pd.read_csv(PATH_FILE)
+
+data = {row.letter:row.code for (index, row) in file.iterrows()}
+print(data)
+
+
--- 13_100_days_of_code/Day_21_to_30/Day_26/lesson_04/main.py
+++ 13_100_days_of_code/Day_21_to_30/Day_26/lesson_04/main.py
@@ -10,7 +10,7 @@ student_dict = {
 
 student_data_frame = pd.DataFrame(student_dict)
 
-print(student_data_frame)
+# print(student_data_frame)
 
 # loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
