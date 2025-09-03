19d6c8ee8919eab8a0c48e0e47d658cf1bf0248a
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-03 07:57:51 -0300

    sending emails

---
+++ 13_100_days_of_code/Day_31_to_40/Day_32/main.py
@@ -0,0 +1,22 @@
+import smtplib
+
+my_email = 'gustavo.a.t.m.12345@hotmail.com'
+password = 'xobdxcadxfukjsrd'
+
+
+try:
+    connection = smtplib.SMTP("smtp.office365.com", 587)
+    connection.ehlo()
+    connection.starttls()
+    connection.ehlo()
+    connection.login(my_email, password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="destinatario@hotmail.com",
+        msg="Subject: Prueba\n\nEste es un correo enviado con contraseña de aplicación."
+    )
+    print("Correo enviado exitosamente.")
+except smtplib.SMTPAuthenticationError as e:
+    print("Error de autenticación:", e)
+finally:
+    connection.quit()
