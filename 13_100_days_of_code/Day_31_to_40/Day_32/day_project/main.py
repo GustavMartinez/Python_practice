825268d29e6d8acf020229ca307d68858b11d043
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-05 07:49:46 -0300

    day 32

---
+++ 13_100_days_of_code/Day_31_to_40/Day_32/day_project/main.py
@@ -0,0 +1,35 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+import pandas as pd
+import datetime as dt
+
+DATA_PATH = '/home/gustavo/Documents/Study/005_Python_General/github/python_practice/13_100_days_of_code/Day_31_to_40/Day_32/day_project/birthdays.csv'
+
+
+
+
+# Read database
+data = pd.read_csv(DATA_PATH)
+
+# Create an object to know the month and day
+now = dt.datetime.now()
+month = now.month
+day = now.day
+
+
+dicts = data.to_dict(orient='list')
+
+for k, v in dicts.items():
+    print(v)
+
+
+
