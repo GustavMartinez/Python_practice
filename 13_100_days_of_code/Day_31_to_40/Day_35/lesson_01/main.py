41faed8b94c6e9cc0a4f5d3221ac2ac5b6ce2c6b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-09 20:21:06 -0300

    working with API, requests and json data

---
+++ 13_100_days_of_code/Day_31_to_40/Day_35/lesson_01/main.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+# Save the response in:
+
+PATH_TO_SAVE = '/home/gustavo/Documents/Study/005_Python_General/github/python_practice/13_100_days_of_code/Day_31_to_40/Day_35/lesson_01/'
+
+# To use the API I need to create an API key
+API_KEY = '937d94de74dac20e86fdce71e0eda333'
+
+# The API works with the latitude and longitude, so I need those values
+LATITUDE = -19.918180
+LONGITUDE = -43.937050
+
+parameters = {
+    'lat': LATITUDE,
+    'lon': LONGITUDE,
+    'appid': API_KEY
+}
+
+#response = requests.get(url='https://api.openweathermap.org/data/2.5/forecast?lat=-19.918180&lon=-43.937050&appid=937d94de74dac20e86fdce71e0eda333')
+
+# Make the request
+response = requests.get(url='https://api.openweathermap.org/data/2.5/forecast', params=parameters)
+
+# Raise the status
+response.raise_for_status()
+
+# get the status
+status = response.status_code
+
+# save the json response to a variable
+data = response.json()
+
+# print the status
+print(status)
+
+# Save the response to a json file:
+with open(f"{PATH_TO_SAVE}data.json", 'w') as file:
+    json.dump(data, file, indent=4)
