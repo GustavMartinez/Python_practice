997c4137ee0dd1e2e93a263993fa23b074ea3604
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-12 22:29:26 -0300

    day 3 update

--- 13_100_days_of_code/Day_01_to_10/Day_03/lesson_06.py
+++ 13_100_days_of_code/Day_01_to_10/Day_03/lesson_06.py
@@ -9,31 +9,31 @@
 
 print("Welcome to Python Pizza Deliveries!")
 
-size = input("What size pizza de you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+size = input("What size pizza de you want? S, M or L: ").upper()
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
 
 final_bill = 0
 
-if size == "s" or size == "S":
+if size == "S":
     final_bill = 15
-    if pepperoni == "y" or pepperoni =="Y":
+    if pepperoni =="Y":
         final_bill += 2
-    if extra_cheese == "y" or extra_cheese == "Y":
+    if extra_cheese == "Y":
         final_bill += 1
 
-elif size == "m" or size == "M":
+elif size == "M":
     final_bill = 20
-    if pepperoni == "y" or pepperoni == "Y":
+    if pepperoni == "Y":
         final_bill += 3
-    if extra_cheese == "y" or extra_cheese == "Y":
+    if extra_cheese == "Y":
         final_bill += 1
 
-elif size == "l" or size == "L":
+elif size == "L":
     final_bill = 25
-    if pepperoni == "y" or pepperoni == "Y":
+    if pepperoni == "Y":
         final_bill += 3
-    if extra_cheese == "y" or extra_cheese == "Y":
+    if extra_cheese == "Y":
         final_bill += 1
 
 else:
