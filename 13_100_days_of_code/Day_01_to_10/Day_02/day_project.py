8edf54bded3e77a40993e4103c5fb0a08d30e109
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-12 21:51:56 -0300

    update day 1 and 2

--- 13_100_days_of_code/Day_01_to_10/Day_02/day_project.py
+++ 13_100_days_of_code/Day_01_to_10/Day_02/day_project.py
@@ -1,9 +1,18 @@
 print("Welcome to the tip calculator!")
 
-total_bill = float(input("What was the total bill? \n$"))
-tip = float(input("How much tip would you like to give? 10, 12, or 15? \n"))
-num_people = int(input("How many people to split the bill? \n"))
+try:
+    total_bill = float(input("What was the total bill? \n$"))
+    tip = float(input("How much tip would you like to give? 10, 12, or 15? \n"))
+    num_people = int(input("How many people to split the bill? \n"))
 
-bill_each_person = (total_bill + (total_bill*(tip/100)))/num_people
+except ValueError:
+    print('Not a valid value')
 
-print(f"Each person should pay: ${round(bill_each_person, 2)}")
+except ZeroDivisionError:
+    print("Division by zero not allow")
+
+
+else:
+    bill_each_person = (total_bill + (total_bill*(tip/100)))/num_people
+
+    print(f"Each person should pay: ${round(bill_each_person, 2)}")
