a368204f83fd23518d12bfb34922833eefffe2ee
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-15 20:29:21 -0300

    comments in the code

--- 13_100_days_of_code/Day_01_to_10/Day_07/lesson_01.py
+++ 13_100_days_of_code/Day_01_to_10/Day_07/lesson_01.py
@@ -10,15 +10,20 @@ TO DO-3 Check if the letter the user guessed `guess` is one of the letters in th
 
 import random
 
+# list of words to guess
 word_list = ["aardvark", "baboon", "camel"]
 
+# create a variable with a randon word of the list of words
 chosen_word = random.choice(word_list)
+
 
 print(chosen_word)
 
+# ask the user for a letter and convert it to lower
 guess = input("Guess a letter: ").lower()
 
 
+# evaluate if the letter is in the chosen word
 for letter in chosen_word:
     if guess == letter:
         print("Right")
