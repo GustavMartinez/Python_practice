2a9646685c5d360224dda48c5491196a81ae8e8b
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-09-24 07:21:27 -0300

    day 16 update

--- 13_100_days_of_code/Day_11_to_20/Day_16/lesson_01.py
+++ 13_100_days_of_code/Day_11_to_20/Day_16/lesson_01.py
@@ -1,7 +1,13 @@
 # OOP
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 mi_tortu = Turtle()
+mi_tortu.color('red')
+mi_tortu.forward(150)
 
 print(mi_tortu)
+
+
+my_screen = Screen()
+my_screen.exitonclick()
