0a872d1c2efc2375e09daaf74aa8fa0f1445f7c5
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2025-11-18 19:09:17 -0300

    intermediate python for developers - intro

---
+++ 15_intermediate_python_for_devs_datacamp/chapter_01/main_modules.py
@@ -0,0 +1,24 @@
+"""
+Modules are Python scripts
+
+- Files ending with .py
+- Contain functions and attributes
+- Can contain other modules
+- Help us avoid rewriting code that already exists
+
+- There are around 200 built-in modules
+
+- Two more popular: OS, string
+
+OS module:
+
+- Used for interacting with our operating system
+- Check the current directory
+- List available files
+- Access environment variables
+
+String module:
+
+- simplifies text processing tasks
+
+"""
