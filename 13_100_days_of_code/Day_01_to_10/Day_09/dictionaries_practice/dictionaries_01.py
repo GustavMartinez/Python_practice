95fcea535d7258b1f76fb81049cd74a23bdb4bae
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-04 20:02:18 -0300

    ejercicio de diccionarios

---
+++ 13_100_days_of_code/Day_01_to_10/Day_09/dictionaries_practice/dictionaries_01.py
@@ -0,0 +1,40 @@
+
+"""
+
+Ejercicio 1:
+Crea un diccionario llamado persona con las claves: "nombre", "edad" y "ciudad".
+
+Asigna valores a cada clave.
+
+Imprime solo el valor asociado a "ciudad".
+
+Cambia la edad por un nuevo valor.
+
+Agrega una nueva clave "profesion" con algún valor.
+
+Imprime el diccionario final.
+
+Cuando lo termines, me pasas tu código.
+
+"""
+
+
+
+
+persona = {
+    "nombre" : "Miguel",
+    "edad" : 15,
+    "ciudad" : "Mariana",
+}
+
+# tarea 2
+print(persona["ciudad"])
+
+# tarea 3
+persona["edad"] = 25
+
+# tarea 4
+persona["profesion"] = "Politico"
+
+# tarea 5
+print(persona)
