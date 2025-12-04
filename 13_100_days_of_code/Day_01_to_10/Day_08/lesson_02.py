00e45785a08e068f125328b65b9869119588cfce
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2025-12-04 16:31:14 -0300

    positional and keyword arguments update

--- 13_100_days_of_code/Day_01_to_10/Day_08/lesson_02.py
+++ 13_100_days_of_code/Day_01_to_10/Day_08/lesson_02.py
@@ -2,6 +2,9 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"What is it like in {location} ")
 
-
+# positional arguments
 greet_with("Gustavo", "Belo horizonte")
 greet_with("Belo horizonte","Gustavo")
+
+# keyword arguments
+greet_with(location="Brasilia", name="Gabriela")
