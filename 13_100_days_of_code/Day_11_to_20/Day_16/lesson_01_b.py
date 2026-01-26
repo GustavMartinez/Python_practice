a188b409a3eb7a9a538473dcf04895549ad4eedb
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-26 18:55:38 -0300

    review lesson 16

---
+++ 13_100_days_of_code/Day_11_to_20/Day_16/lesson_01_b.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+jimy = Turtle()
+jimy.shape('turtle')
+jimy.color('red')
+jimy.forward(100)
+
+my_screen = Screen()
+my_screen.canvheight = 100
+my_screen.canvwidth = 100
+
+my_screen.exitonclick()
+
