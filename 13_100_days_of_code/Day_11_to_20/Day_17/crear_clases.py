e08fb43485092ea80a1a2344bdf61699c0b09f3b
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-01-26 19:36:09 -0300

    adicion de metodos a la clase

--- 13_100_days_of_code/Day_11_to_20/Day_17/crear_clases.py
+++ 13_100_days_of_code/Day_11_to_20/Day_17/crear_clases.py
@@ -1,10 +1,22 @@
 class User:
-    def __init__(self, user_id, user_name):
+    def __init__(self, user_id, user_name, user_age):
         self.id = user_id
         self.user_name = user_name
+        self.age = user_age
+        self.followers = 0
+
+    def new_follower(self):
+        self.followers +=1
 
 
-user_1 = User("012", "Gustavo")
+user_1 = User("012", "Gustavo", 19)
 
 print(user_1.id)
 print(user_1.user_name)
+print(user_1.age)
+print(user_1.followers)
+
+user_1.new_follower()
+user_1.new_follower()
+user_1.new_follower()
+print(f"Total de followers: {user_1.followers}")
